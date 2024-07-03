aec68bb4bae43969b216e80d844e128750539539
Finn <>
2024-07-03 17:55:04 +0800

    feat: Init

---
+++ Nihongo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nihongo
+//
+//  Created by Finn on 2024/7/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
